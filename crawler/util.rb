a753d0cb0e2c17a4dad8d7dd4cc3e94674f9430c
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-09-11 19:39:19 -0300

    Novas atualizações

--- crawler/util.rb
+++ crawler/util.rb
@@ -2,6 +2,7 @@ require 'uri/http'
 require 'cgi'
 require 'rest_client'
 require 'nokogiri'
+require 'open-uri'
 
 def get_field(field, element)
 	# TODO: try dymanic calling
@@ -23,7 +24,7 @@ def get_name(element)
 end
 
 def get_lattes(element)
-	unless element[:href] == 'http://lattes.cnpq.br/'
+	unless element == 'undefined' || element[:href] == 'http://lattes.cnpq.br/'
 		element[:href]
 	else
 		'undefined'
@@ -52,7 +53,26 @@ end
 
 def drop_empty_names(values)
 	values.reject do |element|
-		[" ", "PROFESSORES CO-ORIENTADORES", "Docente", ""].include? get_name(element)  #UECE
+		[
+			" ",
+			"PROFESSORES CO-ORIENTADORES",
+			"Docente",
+			"",
+			"Sala:",
+			"E-mail:",
+			"Professor",
+			"orientar D.Sc.?",
+		 	"temporario01", #regex TODO
+			"temporario02", #regex TODO
+			"temporario03", #regex TODO
+			"temporario04", #regex TODO
+			"colaborador05", #regex TODO
+			"Nelson Castro Machado",
+			"http://www.meira.com",
+			"www.engenhariadevendas.com.br",
+			"http://aisapereira.blogspot.com",
+			" O mestrado profissional é formado por:",
+		].include? get_name(element)  #UECE
 	end
 end
 
@@ -68,11 +88,29 @@ def extract_id_lattes(url)
 end
 
 def get_info_from_lattes_page(name)
+	name = name.tr(
+		"ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+		"AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
+	)
+	name.sub! " de", ""
+	name.sub! " da", ""
+	name.sub! " M.", ""
+	name.sub! " B.", ""
+	name.sub! "d´", "d'"
+	name.sub! "  ", " "
+	name.sub! "Roberto Souto Maior de Barros Filho", "Roberto Souto Maior de Barros" #ufpe
+	name.sub! " (Titular)",""
+	name.sub! "colaborador01 - ", ""
+	name.sub! "colaborador02 - ", ""
+	name.sub! "colaborador03 - ", ""
+	name.sub! "colaborador04 - ", ""
+	name.sub! "colaborador07 - ", ""
+	name.sub! " - Ph.D. (VTT, Finlandia, 1982)atividades no PPGI", ""
 	info = {}
 	t = Thread.new do
 		page = RestClient.post(
 			'http://buscatextual.cnpq.br/buscatextual/busca.do', 
-			'metodo=buscar&filtros.buscaNome=true&textoBusca='+name
+			'metodo=buscar&filtros.buscaNome=true&buscarDoutores=true&textoBusca='+name
 		)
 		page = Nokogiri::HTML(page)
 		result = page.css('div.resultado li:has(b a)')
@@ -82,7 +120,10 @@ def get_info_from_lattes_page(name)
 			info[:id10] = link['href'].split('\'')[1]
 		else
 			result.each{|r|
-				if r.content.strip.include? 'Computação'
+				# puts r.css('b a')[0].content.strip
+				# puts r.css('b a')[0]['href']
+				# puts r.content.strip
+				if ['Computação', 'Doutorado em Medicina (Clínica Médica) - Ribeirão Preto'].any? { |word| r.content.strip.include?(word) } 
 					link = r.css('b a')[0]
 					info[:name] = link.content.strip 
 					info[:id10] = link['href'].split('\'')[1]
@@ -96,17 +137,26 @@ def get_info_from_lattes_page(name)
 end
 
 def extract_info_from_field_lattes(url, name)
-	# puts url, info, name
-	if(url == 'undefined')
+	# puts url, name
+	if(url == 'undefined' || !url.include?("cnpq.br") || (url.include?("buscatextual") && !url.include?("id")))
 		# return rand(1..1000)
 		# return get_info_from_lattes_page(name)[:id10]
-		url = 'http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id='+get_info_from_lattes_page(name)[:id10]
+		info = get_info_from_lattes_page(name)
+		if info == {}
+			puts "Não foi encotrado na base de Doutores do Lattes"
+			return nil 
+		end
+		url = 'http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id='+info[:id10]
 	end
 	infos = {}
-	uri = URI.parse(url)
+	# uri = URI.parse(url)
+	format = "http://buscatextual.cnpq.br/buscatextual/visualizacv.jsp"
+	if url.include? format
+		url.sub! format, "http://buscatextual.cnpq.br/buscatextual/visualizacv.do"
+	end
 	# TODO corrigir thread
 	t = Thread.new do
-		lattes = Nokogiri::HTML(open(url))
+		lattes = Nokogiri::HTML(open(url.strip))
 
 		content = lattes.css('li:has(span.icone-informacao-autor.img_link)')[0].content.strip
 		infos[:id16] = content.split('http://lattes.cnpq.br/')[1]
