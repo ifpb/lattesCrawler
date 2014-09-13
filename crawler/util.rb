2531c72d7bc4c0debb5463925e0e057ecb2e408d
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-09-13 10:31:43 -0300

    multithread

--- crawler/util.rb
+++ crawler/util.rb
@@ -4,6 +4,11 @@ require 'rest_client'
 require 'nokogiri'
 require 'open-uri'
 
+# TODO 
+# gets_...
+# clean names (\r\n\t)
+# clean puts
+
 def get_field(field, element)
 	# TODO: try dymanic calling
 	if(field.include? 'name')
@@ -28,7 +33,6 @@ def get_lattes(element)
 		element[:href]
 	else
 		'undefined'
-		# 'http://lattes.cnpq.br/'+get_info_from_lattes_page(name)[:id10]
 	end
 end
 
@@ -44,11 +48,101 @@ def get_research_line(element)
 	element.children[2].content.strip #UFG
 end
 
-# TODO gets...
-
 def get_url(url)
 	uri = URI.parse(url)
 	uri.scheme+'://'+uri.host
+end
+
+def extract_researchers_info(idProgram, doc, programInfo)
+
+	programResearchersInfo = []
+
+	unless idProgram == "15001016047P9" # UFPA PDF
+		fields = {}
+
+		programInfo['crawlerSchema'].each { |field|
+			next if field[0] == 'embedded'
+			
+			if field[0] == 'lattes' && field[1] == ""
+				length = fields['names'].length
+				fields['lattes'] = Array.new(length){|i| "undefined"}
+				next 
+			end
+			
+			fields[field[0]] = doc.css(field[1])
+			
+			if field[0] == 'names'
+				fields['names'] = drop_empty_names(fields['names'])
+			end
+		}	
+
+		threadsField = []
+		fields['names'].each_with_index do |name, index|
+			researcher = {}
+			
+			programInfo['crawlerSchema'].keys.each do |field|
+				if(field != 'embedded')
+					# puts field, fields[field][index].inspect
+					researcher[field] = get_field(field, fields[field][index])
+					# puts researcher[field]
+				else
+					# Open researcher homepage
+					threadsField << Thread.new do
+						print '?'
+						if name[:href] == "http://rocha.ucpel.tche.br/" #UFRGS
+							name[:href] = "/https://www.google.com/" # não tem lattes
+						end
+
+						path = name[:href]
+						path = "/"+path unless path[0] == "/"
+						url = get_url(programInfo['homepage'])+path
+						url = "http://mdcc.ufc.br/static"+path[2..-1] if programInfo['homepage'] == "http://mdcc.ufc.br/"
+						url = path[1..-1] if programInfo['homepage'] == "http://ppgc.inf.ufrgs.br/"
+						url = "http://www.ic.unicamp.br"+path if programInfo['homepage'] == "http://www.ic.unicamp.br/pos"
+						url = path[1..-1] if programInfo['homepage'] == "http://w3.ufsm.br/ppgi/"
+						url = "http://ppgcc.dc.ufscar.br/pessoas/docentes-1"+path if programInfo['homepage'] == "http://ppgcc.dc.ufscar.br/"
+						url = "http://www.din.uem.br"+path[6..-1] if programInfo['homepage'] == "http://www.din.uem.br/pos-graduacao/mestrado-em-ciencia-da-computacao/"
+						# puts url
+						
+						begin
+							homepage = Nokogiri::HTML(open(url))
+							programInfo['crawlerSchema']['embedded'].each do |field|
+								if(homepage.css(field[1]).size != 0)
+									value = get_field(field[0], homepage.css(field[1])[0])
+									if(field[0] == 'name')
+										researcher['names'] = value
+									elsif(field[0] == 'lattes2')
+										if(value != 'undefined')
+											researcher['lattes'] = value
+										end
+									else
+										researcher[field[0]] = value
+									end
+								else
+									if(field[0] != 'lattes2')
+										researcher[field[0]] = 'undefined'
+									end
+								end
+								# puts researcher[field[0]]
+							end	
+						rescue
+							researcher['lattes'] = 'undefined'
+						end
+					end
+				end
+			end
+			programResearchersInfo << researcher
+		end
+
+		threadsField.each do |t|
+			t.join
+			print '!'
+		end
+	else # UFPA PDF
+		programResearchersInfo = JSON.parse(File.read("lattes-ufpa.json"))
+	end
+
+	programResearchersInfo
 end
 
 def drop_empty_names(values)
@@ -129,6 +223,7 @@ def get_info_from_lattes_page(name)
 	name.sub!(/ \(.*\)/, "")
 	name.sub! /colaborador0\d/, ""
 	name.sub! "Durante os 3 anos seu PhD", ""
+	name.sub! "Leslie Richard Foulds", "Les Foulds"
 	name.sub! "Professor Departamento Ciencia Computacao Universidade Brasilia", ""
 	name.sub! "Ronaldo Farias Ramos", "Ronaldo Fernandes Ramos" #uece
 	name.sub! "Maria Giovanise Oliveira Pontes", "Maria Gilvanise Oliveira Pontes" #uece
@@ -140,14 +235,15 @@ def get_info_from_lattes_page(name)
 	name.sub! "Dr. Joao Mello Silva", "Joao Mello Silva"#unb
 	name.sub! "Jussara Marques Almeida Goncalves", "Jussara Marques Almeida"
 	name.sub! "Roberto Souto Maior de Barros Filho", "Roberto Souto Maior de Barros" #ufpe
-	puts
-	puts name
-	puts
+	# puts
+	# puts name
+	# puts
 	info = {}
-	t = Thread.new do
+	# t = Thread.new do
 		page = RestClient.post(
 			'http://buscatextual.cnpq.br/buscatextual/busca.do', 
-			'metodo=buscar&filtros.buscaNome=true&buscarDoutores=true&textoBusca='+name
+			'metodo=buscar&filtros.buscaNome=true&buscarDoutores=true&textoBusca='+name,
+			:verify_ssl => false
 		)
 		page = Nokogiri::HTML(page)
 		result = page.css('div.resultado li:has(b a)')
@@ -174,46 +270,41 @@ def get_info_from_lattes_page(name)
 			}
 		end
 		print '.'
-	end
-	t.join
+	# end
+	# t.join
 	info
 end
 
 def extract_info_from_field_lattes(url, name)
-	# puts url, name
 	if(url == 'undefined' || !url.include?("cnpq.br") || (url.include?("buscatextual") && !url.include?("id")))
-		# return rand(1..1000)
-		# return get_info_from_lattes_page(name)[:id10]
 		info = get_info_from_lattes_page(name)
 		if info == {}
-			puts "Não foi encotrado na base de Doutores do Lattes"
+			# puts "Não foi encotrado na base de Doutores do Lattes"
+			print "x"
 			return nil 
 		end
 		url = 'http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id='+info[:id10]
 	end
 	infos = {}
-	# uri = URI.parse(url)
 	format = "http://buscatextual.cnpq.br/buscatextual/visualizacv.jsp"
 	if url.include? format
 		url.sub! format, "http://buscatextual.cnpq.br/buscatextual/visualizacv.do"
 	end
 	# TODO corrigir thread
-	t = Thread.new do
+	# t = Thread.new do
 		lattes = Nokogiri::HTML(open(url.strip))
 
 		content = lattes.css('li:has(span.icone-informacao-autor.img_link)')[0].content.strip
 		infos[:id16] = content.split('http://lattes.cnpq.br/')[1]
-# 	infos[:id16] = uri.path[1..-1] if(uri.path.length == 17)
 
-		# TODO lattes local & name lattes
 		# TODO name local & name lattes
 		infos[:name] = lattes.css('h2.nome')[0].content.strip
 
 		# TODO images
 		infos[:image] = lattes.css('img.foto')[0][:src]
 
-		puts '.'
-	end
-	t.join
+		print '.'
+	# end
+	# t.join
 	infos
 end
